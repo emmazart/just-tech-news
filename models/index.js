8458ba3e0494797c65d9855ef87c3ff2b3b90f17
Emma Olson <emma.olson794@gmail.com>
2022-05-30 11:41:29 -0500

    write Vote model

--- models/index.js
+++ models/index.js
@@ -1,6 +1,8 @@
 const User = require('./User');
 const Post = require('./Post');
+const Vote = require('./Vote');
 
+// direct relationships between user and post
 // create associations - one to many
 User.hasMany(Post, {
     foreignKey: 'user_id'
@@ -10,4 +12,35 @@ Post.belongsTo(User, {
     foreignKey: 'user_id',
 });
 
-module.exports = { User, Post };
+
+// establish through relationship between Post & User through Vote
+User.belongsToMany(Post, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'user_id'
+});
+
+Post.belongsToMany(User, {
+    through: Vote,
+    as: 'voted_posts',
+    foreignKey: 'post_id'
+});
+
+// Direct relationships between Vote & User / Vote & Post
+Vote.belongsTo(User, {
+    foreignKey: 'user_id'
+});
+
+Vote.belongsTo(Post, {
+    foreignKey: 'post_id'
+});
+
+User.hasMany(Vote, {
+    foreignKey: 'user_id'
+});
+
+Post.hasMany(Vote, {
+    foreignKey: 'post_id'
+});
+
+module.exports = { User, Post, Vote };
